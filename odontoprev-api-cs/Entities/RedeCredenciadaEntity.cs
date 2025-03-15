edabdddcafa5932353712be59fe0822d248b735f
Ives Chiba <ivesjundi@gmail.com>
2025-03-15 09:11:58 -0300

    feat all entities

---
+++ odontoprev-api-cs/Entities/RedeCredenciadaEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace odontoprev_api_cs.Entities
+{
+    [Table("tb_rede_credenciada")]
+    public class RedeCredenciadaEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Nome { get; set; }
+
+        public virtual ICollection<EnderecoEntity>? Enderecos { get; set; }
+        public virtual ICollection<PrestadorServicoEntity>? PrestadorServicos { get; set; }
+    }
+}
---
+++ odontoprev-api-cs/Entities/TipoServicoEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace odontoprev_api_cs.Entities
+{
+    [Table("tb_tipo_servico")]
+    public class TipoServicoEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Nome { get; set; }
+
+        [Required]
+        public required double ValorReais { get; set; }
+
+        public virtual ICollection<TipoPlanoEntity>? TipoPlanos { get; set; }
+        public virtual ICollection<ServicoEntity>? Servicos { get; set; }
+        public virtual ICollection<PrestadorServicoEntity>? PrestadorServicos { get; set; }
+    }
+}
